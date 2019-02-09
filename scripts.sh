596d405699db56432540e37e9f8c68db4babf3ca
worasit <worasit@users.noreply.github.com>
2019-02-09 20:15:14 +0700

    Added the tutorial scripts

---
+++ scripts.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+docker run -d -p 2181:2181 -p 3030:3030 -p 8081-8083:8081-8083 \
+           -p 9581-9585:9581-9585 -p 9092:9092 \
+           -e ADV_HOST=localhost \
+           -e RUNTESTS=1 \
+           -e RUNNING_SAMPLEDATA=1 \
+           landoop/fast-data-dev
